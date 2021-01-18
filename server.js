e8a8df239ef7bc6cc2ed6a655b2b03f60d392466
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2021-01-17 22:13:14 -0500

    prepared for deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -10,5 +11,15 @@ app.use(express.json());
 const ontarioRoute = require('./routes/ontario');
 
 app.use('/api/ontario', ontarioRoute);
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => console.log(`Server Started on Port: ${PORT}`));
