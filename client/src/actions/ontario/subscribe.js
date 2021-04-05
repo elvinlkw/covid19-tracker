87dbae243043b8c83a4abf2d028f8dc27ecb47d6
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2021-04-05 18:55:06 -0400

    removed unused code and console logs

--- client/src/actions/ontario/subscribe.js
+++ client/src/actions/ontario/subscribe.js
@@ -38,7 +38,6 @@ export const unsubscribe = (email, token) => async dispatch => {
     dispatch(subSuccess());
   } catch (error) {
     const errors = error.response.data.errors;
-    console.log(error.response.data);
     if(errors) {
       errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
     }
--- client/src/actions/subscribe.js
+++
@@ -1,9 +0,0 @@
-import { SUB_SUCCESS, SUB_RESET } from './types';
-
-export const subSuccess = () => {
-  return { type: SUB_SUCCESS };
-}
-
-export const subReset = () => {
-  return { type: SUB_RESET };
-}
